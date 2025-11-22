417b4f8da0ea16b60493d8ac9a34a8ee9b3a3662
deku99hh <omar.rashed1000@gmail.com>
2025-11-22 19:08:09 +0200

    function clickingGrades(choice,block,slice){     console.log(TheArray[block].slices[slice].grade);     TheArray[block].slices[slice].grade=choice;     console.log(TheArray);     console.log(TheArray[block].slices[slice].grade); };
    function clickingCredit(choice,block,slice){
        console.log(TheArray[block].slices[slice].credit);
        TheArray[block].slices[slice].credit=choice;
        console.log(TheArray);
        console.log(TheArray[block].slices[slice].credit);
    };
    
    function clickingIsAgain(choice,block,slice){
        console.log(TheArray[block].slices[slice].isAgain);
        TheArray[block].slices[slice].isAgain=choice;
        console.log(TheArray);
        console.log(TheArray[block].slices[slice].isAgain);
    };

--- script.js
+++ script.js
@@ -138,47 +138,47 @@ function update(){
                 <input type="checkbox" name="grade" id="checkboxGrade">
                 
                 <div class="gradePOPup POPup">
-                    <button>A</button>
-                    <button>A-</button>
-                    <button>B+</button>
-                    <button>B</button>
-                    <button>B-</button>
-                    <button>C+</button>
-                    <button>C</button>
-                    <button>C-</button>
-                    <button>D+</button>
-                    <button>D</button>
-                    <button>D-</button>
-                    <button>F</button>
+                    <button onclick="clickingGrades('A',${Number(semesterIndex)},${Number(sliceIndex)})">A</button>
+                    <button onclick="clickingGrades('A-',${Number(semesterIndex)},${Number(sliceIndex)})">A-</button>
+                    <button onclick="clickingGrades('B+',${Number(semesterIndex)},${Number(sliceIndex)})">B+</button>
+                    <button onclick="clickingGrades('B',${Number(semesterIndex)},${Number(sliceIndex)})">B</button>
+                    <button onclick="clickingGrades('B-',${Number(semesterIndex)},${Number(sliceIndex)})">B-</button>
+                    <button onclick="clickingGrades('C+',${Number(semesterIndex)},${Number(sliceIndex)})">C+</button>
+                    <button onclick="clickingGrades('C',${Number(semesterIndex)},${Number(sliceIndex)})">C</button>
+                    <button onclick="clickingGrades('C-',${Number(semesterIndex)},${Number(sliceIndex)})">C-</button>
+                    <button onclick="clickingGrades('D+',${Number(semesterIndex)},${Number(sliceIndex)})">D+</button>
+                    <button onclick="clickingGrades('D',${Number(semesterIndex)},${Number(sliceIndex)})">D</button>
+                    <button onclick="clickingGrades('D-',${Number(semesterIndex)},${Number(sliceIndex)})">D-</button>
+                    <button onclick="clickingGrades('F',${Number(semesterIndex)},${Number(sliceIndex)})">F</button>
                 </div>
                 <!-- جبت كام -->
 
                 <input type="checkbox" name="credit" id="checkboxCredit"> 
                 <div class="creditPOPup POPup">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
+                    <button onclick="clickingCredit(1,${Number(semesterIndex)},${Number(sliceIndex)})">1</button>
+                    <button onclick="clickingCredit(2,${Number(semesterIndex)},${Number(sliceIndex)})">2</button>
+                    <button onclick="clickingCredit(3,${Number(semesterIndex)},${Number(sliceIndex)})">3</button>
+                    <button onclick="clickingCredit(4,${Number(semesterIndex)},${Number(sliceIndex)})">4</button>
+                    <button onclick="clickingCredit(5,${Number(semesterIndex)},${Number(sliceIndex)})">5</button>
+                    <button onclick="clickingCredit(6,${Number(semesterIndex)},${Number(sliceIndex)})">6</button>
                 </div>
                 <!-- عدد الساعات -->
 
                 <input type="checkbox" name="isAgain" id="checkboxIsAgain">
                 <div class="isAgainPOPup POPup">
-                    <button>NO</button>
-                    <button>A</button>
-                    <button>A-</button>
-                    <button>B+</button>
-                    <button>B</button>
-                    <button>B-</button>
-                    <button>C+</button>
-                    <button>C</button>
-                    <button>C-</button>
-                    <button>D+</button>
-                    <button>D</button>
-                    <button>D-</button>
-                    <button>F</button>
+                    <button onclick="clickingIsAgain('NO',${Number(semesterIndex)},${Number(sliceIndex)})">NO</button>
+                    <button onclick="clickingIsAgain('A',${Number(semesterIndex)},${Number(sliceIndex)})">A</button>
+                    <button onclick="clickingIsAgain('A-',${Number(semesterIndex)},${Number(sliceIndex)})">A-</button>
+                    <button onclick="clickingIsAgain('B+',${Number(semesterIndex)},${Number(sliceIndex)})">B+</button>
+                    <button onclick="clickingIsAgain('B',${Number(semesterIndex)},${Number(sliceIndex)})">B</button>
+                    <button onclick="clickingIsAgain('B-',${Number(semesterIndex)},${Number(sliceIndex)})">B-</button>
+                    <button onclick="clickingIsAgain('C+',${Number(semesterIndex)},${Number(sliceIndex)})">C+</button>
+                    <button onclick="clickingIsAgain('C',${Number(semesterIndex)},${Number(sliceIndex)})">C</button>
+                    <button onclick="clickingIsAgain('C-',${Number(semesterIndex)},${Number(sliceIndex)})">C-</button>
+                    <button onclick="clickingIsAgain('D+',${Number(semesterIndex)},${Number(sliceIndex)})">D+</button>
+                    <button onclick="clickingIsAgain('D',${Number(semesterIndex)},${Number(sliceIndex)})">D</button>
+                    <button onclick="clickingIsAgain('D-',${Number(semesterIndex)},${Number(sliceIndex)})">D-</button>
+                    <button onclick="clickingIsAgain('F',${Number(semesterIndex)},${Number(sliceIndex)})">F</button>
                 </div>
                 <!-- عايد؟؟ -->
             </div>
@@ -202,6 +202,23 @@ function update(){
     buttonListener();
 };
 
-function clickingGrades(){
+function clickingGrades(choice,block,slice){
+    console.log(TheArray[block].slices[slice].grade);
+    TheArray[block].slices[slice].grade=choice;
+    console.log(TheArray);
+    console.log(TheArray[block].slices[slice].grade);
+};
 
-}
+function clickingCredit(choice,block,slice){
+    console.log(TheArray[block].slices[slice].credit);
+    TheArray[block].slices[slice].credit=choice;
+    console.log(TheArray);
+    console.log(TheArray[block].slices[slice].credit);
+};
+
+function clickingIsAgain(choice,block,slice){
+    console.log(TheArray[block].slices[slice].isAgain);
+    TheArray[block].slices[slice].isAgain=choice;
+    console.log(TheArray);
+    console.log(TheArray[block].slices[slice].isAgain);
+};
